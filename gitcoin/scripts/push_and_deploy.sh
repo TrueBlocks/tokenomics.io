f83b39f1c99b36ebc77065168cc7290cdc3471f5
tjayrush <jrush@quickblocks.io>
2021-11-28 05:07:15 -0500

    Updating deploy scripts

--- gitcoin/scripts/push_and_deploy.sh
+++ gitcoin/scripts/push_and_deploy.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
 git push
-#ssh jrush@unchainedindex.io "cd Development/tokenomics.io/gitcoin ; git pull ; ./scripts/deploy_ui.sh"
-ssh jrush@unchainedindex.io "cd Development/tokenomics.io/gitcoin ; git pull ; ls -l"
+ssh jrush@unchainedindex.io "cd Development/tokenomics.io/gitcoin ; git pull ; ./scripts/deploy_ui.sh"
+#ssh jrush@unchainedindex.io "cd Development/tokenomics.io/gitcoin ; git pull ; ls -l"
