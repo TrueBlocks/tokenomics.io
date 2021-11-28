2ab9e7068f7e3ea1200b6f8b600401bc2a9c2304
tjayrush <jrush@quickblocks.io>
2021-11-28 05:06:16 -0500

    Updating deploy scripts

--- gitcoin/deploy_ui.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-# build the site and deploy it to the website
-
-cd ui
-yarn build
-yes | cp -pr build/* /home/jrush/Websites/tokenomics.io/gitcoin/
-cd - 2>/dev/null
-yes | cp -pR charts/* /home/jrush/Websites/tokenomics.io/gitcoin/charts/
