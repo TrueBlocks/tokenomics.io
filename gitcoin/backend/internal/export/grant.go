32d4c3ee36d899ef0161781e49839d9c10316aeb
tjayrush <jrush@quickblocks.io>
2021-12-01 22:47:12 -0500

    Counts neighbors and logs

--- gitcoin/backend/internal/export/grant.go
+++ gitcoin/backend/internal/export/grant.go
@@ -6,10 +6,12 @@ package exportPkg
 
 import (
 	"bufio"
+	"bytes"
 	"encoding/binary"
 	"encoding/json"
 	"errors"
 	"fmt"
+	"io"
 	"io/ioutil"
 	"os"
 	"strconv"
@@ -149,27 +151,56 @@ type Appearance struct {
 }
 
 type Monitor struct {
-	Id         uint64     `json:"grantId"`
-	Address    string     `json:"address"`
-	Name       string     `json:"name"`
-	Slug       string     `json:"slug"`
-	First      Appearance `json:"firstAppearance"`
-	Latest     Appearance `json:"latestAppearance"`
-	LastUpdate uint64     `json:"lastUpdated"`
-	// Age         uint32     `json:"ageInBlocks"`
-	Range     uint32   `json:"blockRange"`
-	Size      int64    `json:"fileSize"`
-	Count     int64    `json:"appearanceCount"`
-	Neighbors int64    `json:"neighborCount"`
-	Types     string   `json:"types"`
-	LogCount  int64    `json:"logCount"`
-	Matched   float64  `json:"matched"`
-	Claimed   float64  `json:"claimed"`
-	Balances  Balances `json:"balances,omitempty"`
-	Core      bool     `json:"core"`
+	Id            uint64     `json:"grantId"`
+	Address       string     `json:"address"`
+	Name          string     `json:"name"`
+	Slug          string     `json:"slug"`
+	First         Appearance `json:"firstAppearance"`
+	Latest        Appearance `json:"latestAppearance"`
+	LastUpdate    uint64     `json:"lastUpdated"`
+	Range         uint32     `json:"blockRange"`
+	Size          int64      `json:"fileSize"`
+	Count         int64      `json:"appearanceCount"`
+	NeighborCount int64      `json:"neighborCount"`
+	Types         string     `json:"types"`
+	LogCount      int64      `json:"logCount"`
+	Matched       float64    `json:"matched"`
+	Claimed       float64    `json:"claimed"`
+	Balances      Balances   `json:"balances,omitempty"`
+	Core          bool       `json:"core"`
 }
 
-func (m *Monitor) ReadRangeAndAge(monitorPath string) error {
+func CountLines(path string) (int64, error) {
+	file, err := os.Open(path)
+	if err != nil {
+		return 0, err
+	}
+	defer file.Close()
+	cnt, err := LineCounter(file)
+	if err != nil {
+		return 0, err
+	}
+	return int64(cnt), nil
+}
+
+func LineCounter(r io.Reader) (int, error) {
+	buf := make([]byte, 32*1024)
+	count := 0
+	lineSep := []byte{'\n'}
+	for {
+		c, err := r.Read(buf)
+		count += bytes.Count(buf[:c], lineSep)
+		switch {
+		case err == io.EOF:
+			return count, nil
+		case err != nil:
+			fmt.Println("I am here", err.Error())
+			return count, err
+		}
+	}
+}
+
+func (m *Monitor) ReadRange(monitorPath string) error {
 	monitorFile, err := os.Open(monitorPath)
 	if err != nil {
 		return err
@@ -202,8 +233,6 @@ func (m *Monitor) ReadRangeAndAge(monitorPath string) error {
 	m.Latest.Date = strings.Replace(m.Latest.Date, "T", " ", -1)
 
 	m.Range = m.Latest.Bn - m.First.Bn + 1
-	// var meta rpcClient.Meta
-	// m.Age = uint32(meta.Latest()) - m.First.Bn
 	return nil
 }
 
@@ -250,8 +279,18 @@ func GetMonitorStats(grantId string, grant *Grant) (*Monitor, error) {
 	monitor.Size = fileStat.Size()
 	monitor.Count = fileStat.Size() / 8
 	monitor.Core = strings.Contains(grantId, "core")
+	pp := pathToData + "neighbors/" + monitor.Address + ".csv"
+	monitor.NeighborCount, _ = CountLines(pp)
+	if monitor.NeighborCount > 0 {
+		monitor.NeighborCount -= 1 // for header
+	}
+	pp = pathToData + "logs/" + monitor.Address + ".csv"
+	monitor.LogCount, _ = CountLines(pp)
+	if monitor.LogCount > 0 {
+		monitor.LogCount -= 1 // for header
+	}
 
-	err = monitor.ReadRangeAndAge(monitorPath)
+	err = monitor.ReadRange(monitorPath)
 	if err != nil {
 		return nil, err
 	}
