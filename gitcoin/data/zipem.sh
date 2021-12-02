b7685af530a44de3ba9c34d84e87fb6de74efd62
tjayrush <jrush@greathill.com>
2021-12-02 00:20:12 +0000

    added zipem.sh

---
+++ gitcoin/data/zipem.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+rm -f zips/*
+
+tar -cvf zips/apps.tar apps
+tar -cvf zips/logs.tar logs
+tar -cvf zips/neighbors.tar neighbors
+tar -cvf zips/raw.tar raw
+tar -cvf zips/txs.tar txs
+
+cd zips
+gzip -v *
+ls -l
