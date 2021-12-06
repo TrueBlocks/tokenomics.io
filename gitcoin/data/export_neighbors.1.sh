e521a040d1b6430e3b625cbd6af97cb11984c5c8
tjayrush <jrush@quickblocks.io>
2021-12-06 05:41:43 -0500

    Trying to catch up neighbors processing

--- gitcoin/data/export_neighbors.1.sh
+++ gitcoin/data/export_neighbors.1.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 
 addr=`echo $1 | tr [:upper:] [:lower:]`
-chifra export --neighbors --cache --cache_traces --fmt csv $addr | \
-    sed 's/'$addr'/-----------------self---------------------/' >neighbors/$addr.csv
+chifra list --count $addr
+time chifra export --neighbors --cache --cache_traces --fmt csv $addr >neighbors/$addr.csv
+./fixHeaders $addr
