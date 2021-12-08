fc06939bd1f3fcc1913a6c5c660abe84e793f27e
tjayrush <jrush@quickblocks.io>
2021-12-08 09:21:17 -0500

    Updating update

--- gitcoin/data/updateCmd/update.cpp
+++ gitcoin/data/updateCmd/update.cpp
@@ -69,7 +69,7 @@ int main(int argc, const char* argv[]) {
 
                 ostringstream oss;
                 oss << substitute(STR_CMD1, "[{ADDR}]", addr) << endl;
-                oss << substitute(STR_CMD2, "[{ADDR}]", addr) << endl;
+                // oss << substitute(STR_CMD2, "[{ADDR}]", addr) << endl;
                 oss << substitute(STR_CMD3, "[{ADDR}]", addr) << endl;
                 oss << substitute(substitute(STR_CMD4, "[{ADDR}]", addr), "[{EMITTERS}]", STR_EMIT) << endl;
                 oss << substitute(STR_CMD5, "[{ADDR}]", addr) << endl;
