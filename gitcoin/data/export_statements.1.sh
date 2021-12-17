1a0d09fc3cef383853d66b31dc3f88314e4f1d00
tjayrush <jrush@quickblocks.io>
2021-12-16 22:37:43 -0500

    Updating logs

--- gitcoin/data/export_statements.1.sh
+++ gitcoin/data/export_statements.1.sh
@@ -4,7 +4,7 @@
 addr=`echo $1 | tr [:upper:] [:lower:]`
 
 # generate the data
-#chifra export --statements --fmt csv $addr >statements/$addr.csv
-echo -n $addr ","
-cat statements/$addr.csv | grep -v assetSymbol | cut -f2 -d, | sort | uniq -c | sort -n -r | tr '\n' '|' | sed 's/ //g' | sed 's/\"|/|/g' | tr '"' ';'
-echo
+chifra export --statements --fmt csv $addr >statements/$addr.csv
+#echo -n $addr ","
+#cat statements/$addr.csv | grep -v assetSymbol | cut -f2 -d, | sort | uniq -c | sort -n -r | tr '\n' '|' | sed 's/ //g' | sed 's/\"|/|/g' | tr '"' ';'
+#echo
