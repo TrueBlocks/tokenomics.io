4211e6b752c5ac6883278099c34cd9faf77ac271
tjayrush <jrush@quickblocks.io>
2021-12-17 03:38:46 -0500

    Catching up export_logs

--- gitcoin/data/export_logs.1.sh
+++ gitcoin/data/export_logs.1.sh
@@ -7,7 +7,7 @@ export DISPLAY_LOG="\"[{blockNumber}]\",\"[{transactionIndex}]\",\"[{logIndex}]\
 
 # generate the data
 chifra export \
-    --logs --relevant --cache --cache_traces \
+    --logs --relevant --articulate --cache --cache_traces \
     --emitter 0xdf869fad6db91f437b59f1edefab319493d4c4ce \
     --emitter 0xf2354570be2fb420832fb7ff6ff0ae0df80cf2c6 \
     --emitter 0x7d655c57f71464b6f83811c55d84009cd9f5221c \
