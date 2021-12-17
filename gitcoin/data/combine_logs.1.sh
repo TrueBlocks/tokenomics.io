7c42e1323895e83a6725bff8a845941bcd4cbbf8
tjayrush <jrush@quickblocks.io>
2021-12-17 04:18:26 -0500

    Catching up export_logs

--- gitcoin/data/combine_logs.1.sh
+++ gitcoin/data/combine_logs.1.sh
@@ -5,3 +5,8 @@ cat logs/$1.csv | \
     grep -v -i "^\"block" | \
     sed 's/^/\"'$1'\",/' | \
     tee -a combined/logs.csv
+
+cat logs/articulated/$.txt | \
+    sed 's/^/\"'$1'\",/' | \
+    tee -a combined/logs_articulated.csv
+
