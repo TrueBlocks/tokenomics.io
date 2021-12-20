94396510df9dd2a2d23d01dff1be4311d786af3b
tjayrush <jrush@quickblocks.io>
2021-12-19 22:57:06 -0500

    Fixing zippers

--- gitcoin/data/update_zips.1.sh
+++ gitcoin/data/update_zips.1.sh
@@ -4,11 +4,14 @@
 addr=`echo $1 | tr [:upper:] [:lower:]`
 
 tar -cvf zips/$addr.tar apps/$addr.csv
-#tar -rvf zips/$addr.tar bals/$addr.csv
 tar -rvf zips/$addr.tar txs/$addr.csv
 tar -rvf zips/$addr.tar logs/$addr.csv
-tar -rvf zips/$addr.tar logs/articulated/$addr.txt
+tar -rvf zips/$addr.tar logs/articulated/$addr.csv
 tar -rvf zips/$addr.tar neighbors/$addr.csv
+tar -rvf zips/$addr.tar neighbors/adjacencies/$addr.txt
+tar -rvf zips/$addr.tar statements/$addr.csv
+tar -rvf zips/$addr.tar statements/balances/$addr.csv
+tar -rvf zips/$addr.tar statements/tx_counts/$addr.csv
 
 cd zips
 yes | gzip $addr.tar
