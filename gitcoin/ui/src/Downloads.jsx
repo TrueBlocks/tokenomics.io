94383171b4c6440796c5a8d30eb442c34a758a00
tjayrush <jrush@quickblocks.io>
2021-12-19 23:41:57 -0500

    Fixing downloads page

--- gitcoin/ui/src/Downloads.jsx
+++ gitcoin/ui/src/Downloads.jsx
@@ -19,7 +19,7 @@ export const Downloads = () => {
         <li>Transactions for [ <a target="top" href="https://tokenomics.io/gitcoin/data/zips/combined/txs.csv.gz">all address</a> ]</li>
         <li>Logs for [ <a target="top" href="https://tokenomics.io/gitcoin/data/zips/combined/logs.csv.gz">raw data</a> | <a target="top" href="https://tokenomics.io/gitcoin/data/zips/combined/logs_articulated.csv.gz">articulated</a> ]</li>
         <li>Neighbors for [ <a target="top" href="https://tokenomics.io/gitcoin/data/zips/combined/neighbors.csv.gz">all address</a> ]</li>
-        <li>Neighbors for [ <a target="top" href="https://tokenomics.io/gitcoin/data/zips/combined/statements.csv.gz">raw data</a> | <a target="top" href="https://tokenomics.io/gitcoin/data/zips/combined/statements_balances.csv.gz">summaries</a> | <a target="top" href="https://tokenomics.io/gitcoin/data/zips/combined/statements_tx_counts.csv.gz">asset tx counts</a> ]</li>
+        <li>Statements for [ <a target="top" href="https://tokenomics.io/gitcoin/data/zips/combined/statements.csv.gz">raw data</a> | <a target="top" href="https://tokenomics.io/gitcoin/data/zips/combined/statements_balances.csv.gz">summaries</a> | <a target="top" href="https://tokenomics.io/gitcoin/data/zips/combined/statements_tx_counts.csv.gz">asset tx counts</a> ]</li>
       </ul>
       <ul><b>Note:</b> Data sets containing each individual address are linked on from the main display.</ul>
       <p />
