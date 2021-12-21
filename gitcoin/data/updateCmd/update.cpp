f84b64ab51512729026b0c78389be486f7243384
tjayrush <jrush@quickblocks.io>
2021-12-21 08:01:55 -0500

    Updating updating

--- gitcoin/data/updateCmd/update.cpp
+++ gitcoin/data/updateCmd/update.cpp
@@ -11,37 +11,31 @@
  * Public License along with this program. If not, see http://www.gnu.org/licenses/.
  *-------------------------------------------------------------------------------------------*/
 #include "etherlib.h"
+#include "update.h"
 
-// clang-format off
-const char* STR_CMD_LIST = "chifra export --appearances --fmt csv [{ADDR}] | cut -f2,3 -d',' >apps/[{ADDR}].csv ; ";
-const char* STR_CMD_TXS = "chifra export --articulate --cache --cache_traces --fmt csv [{ADDR}] >txs/[{ADDR}].csv ; ";
-const char* STR_CMD_LOGS = "./export_logs.1.sh [{ADDR}] ; ";
-const char* STR_CMD_NEIGHBORS = "chifra export --neighbors --deep --fmt csv [{ADDR}] >neighbors/[{ADDR}].csv ; ";
-const char* STR_CMD_STATEMENTS = "./export_statements.1.sh [{ADDR}] ; ";
-// clang-format on
-
-//----------------------------------------------------------------
+//-----------------------------------------------------------------------------------
 int main(int argc, const char* argv[]) {
-    CMetaData lastChunk = getMetaData();
+    CStringArray lines;
+    asciiFileToLines("./addresses.csv", lines);
+
+    blknum_t lastChunk = getMetaData().finalized;
     bool first = true;
 
     bool quit = false;
     while (!quit) {
-        CMetaData thisChunk = getMetaData();
-        LOG_INFO("lastChunk: ", lastChunk.finalized, " thisChunk: ", thisChunk.finalized);
-        if (!first && lastChunk.finalized == thisChunk.finalized) {
-            LOG_INFO("Skipping because no new chunks: ", lastChunk.finalized, " --> ", thisChunk.finalized);
-            LOG_INFO("Sleeping for 3 minutes...");
+        timestamp_t start = date_2_Ts(Now());
+
+        blknum_t thisChunk = getMetaData().finalized;
+
+        // We only need to process if this is the first run or we have a new chunk...
+        if (!first && lastChunk == thisChunk) {
+            LOG_INFO("No new chunks: ", lastChunk, " --> ", thisChunk, ". Sleeping for 3 minutes...");
             usleep(1800000000);
             continue;
         }
         first = false;
 
-        CStringArray lines;
-        asciiFileToLines("./addresses.csv", lines);
-
         size_t nChanged = 0, nProcessed = 0;
-        timestamp_t start = date_2_Ts(Now());
         for (auto line : lines) {
             CStringArray parts;
             explode(parts, line, ',');
@@ -51,39 +45,43 @@ int main(int argc, const char* argv[]) {
             if ((!isAddress(addr) || isZeroAddr(addr)))
                 continue;
 
-            nProcessed++;
-
             // Figure out how many records there are...
             string_q monitorFn = getCachePath("monitors/" + addr + ".acct.bin");
             uint64_t nRecordsBefore = fileSize(monitorFn) / 8;
+
+            // If there are too many, report the same and skip...
             if (nRecordsBefore > 100000) {
                 ostringstream os;
-                os << "Skipping massive address: " << addr << " with " << nRecordsBefore << " appearances.";
-                appendToAsciiFile("./skipped-too-large.txt", os.str());
+                os << "Skipping too-large address: " << addr << " with " << nRecordsBefore << " appearances.";
                 LOG_ERR(bRed, os.str(), cOff);
+                appendToAsciiFile("./skipped-too-large.txt", os.str());
                 continue;
             }
 
-            // Freshen the monitor
+            nProcessed++;
+
+            // Freshen the monitor...
             if (system(substitute(STR_CMD_LIST, "[{ADDR}]", addr).c_str()) != 0) {
                 quit = true;
                 break;
             }
 
-            // Figure out how many records there are after freshen
+            // Figure out how many records there are after freshen...
             uint64_t sizeAfter = fileSize(monitorFn) / 8;
 
-            if (fileExists("./apps/" + addr + ".csv") && nRecordsBefore == sizeAfter) {
-                // If there are no new records, we don't have to freshen the rest of the data
+            // If there is no transactions file (we can delete that file to force a re-calc) or
+            // there are new transactions, re-process. Otherwise, skip...
+            if (fileExists("./txs/" + addr + ".csv") && nRecordsBefore == sizeAfter) {
+                // There are no new records, we don't have to freshen the rest of the data...
                 LOG_INFO(bBlack, "Skip ", substitute(monitorFn, getCachePath(""), "./"), bGreen, " (", nRecordsBefore,
                          " == ", sizeAfter, ")", cOff);
 
             } else {
-                nChanged++;
-
-                // There are new records, freshen everything
+                // There are new records, re-write everything...
                 LOG_INFO(bYellow, "Call ", substitute(monitorFn, getCachePath(""), "./"), bGreen, " (", nRecordsBefore,
                          " != ", sizeAfter, ")", cOff);
+
+                nChanged++;
 
                 ostringstream oss;
                 oss << STR_CMD_TXS << endl;
@@ -109,20 +107,28 @@ int main(int argc, const char* argv[]) {
             if (system("./update_zips.sh")) {
             }
         }
-
-        timestamp_t stop = date_2_Ts(Now());
-        ostringstream out;
-        out << lines.size() << "," << nProcessed << "," << nChanged << "," << start << "," << stop << ","
-            << (stop - start) << endl;
-        out << "sleeping: " << date_2_Ts(Now()) << endl;
-        LOG_INFO(out.str());
-        usleep(1800000000);
-        out << "starting: " << date_2_Ts(Now()) << endl;
-        appendToAsciiFile("./timing.txt", out.str());
-
         lastChunk = thisChunk;
 
+        string_q report = reportTiming(lines.size(), nProcessed, nChanged, start);
+        LOG_INFO(report, ". Sleeping...");
+        usleep(1800000000);
     }  // while (true)
 
     return 0;
 }
+
+//-----------------------------------------------------------------------------------
+void reportTiming(size_t nLines, size_t nProc, size_t nChange, timestamp_t start) {
+    timestamp_t stop = date_2_Ts(Now());
+    ostringstream out;
+    out << nLines << "," << nProc << "," << nChange << "," << start << "," << stop << "," << (stop - start) << endl;
+    appendToAsciiFile("./timing.txt", out.str());
+    return out.str();
+}
+
+//-----------------------------------------------------------------------------------
+const char* STR_CMD_LIST = "chifra export --appearances --fmt csv [{ADDR}] | cut -f2,3 -d',' >apps/[{ADDR}].csv ; ";
+const char* STR_CMD_TXS = "chifra export --articulate --cache --cache_traces --fmt csv [{ADDR}] >txs/[{ADDR}].csv ; ";
+const char* STR_CMD_LOGS = "./export_logs.1.sh [{ADDR}] ; ";
+const char* STR_CMD_NEIGHBORS = "chifra export --neighbors --deep --fmt csv [{ADDR}] >neighbors/[{ADDR}].csv ; ";
+const char* STR_CMD_STATEMENTS = "./export_statements.1.sh [{ADDR}] ; ";
