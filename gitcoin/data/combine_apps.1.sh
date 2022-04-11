7b5dc02a830074776e51e3d212768c784768c444
tjayrush <jrush@greathill.com>
2022-04-11 22:38:00 +0000

    Removes old data folder

--- gitcoin/data/combine_apps.1.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-# remove the header, add in a column for the address, append to combined file
-cat apps/$1.csv | \
-    grep -v -i "^\"block" | \
-    sed 's/^/\"'$1'\",/' | \
-    tee -a combined/apps.csv
--- gitcoin/data/combine_neighbors.1.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-# remove the header, add in a column for the address, append to combined file
-cat neighbors/$1.csv | \
-    grep -v -i "^\"block" | \
-    grep -v -i "^\"bn\",\"tx" | \
-    sed 's/^/\"'$1'\",/' | \
-    tee -a combined/neighbors.csv
