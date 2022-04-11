9e9ad9e1a38d93db8df1a2c9907285da34686acf
tjayrush <jrush@greathill.com>
2022-04-11 22:29:34 +0000

    Adding files to scripts folder

---
+++ gitcoin/scripts/combine_logs.1.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+# remove the header, add in a column for the address, append to combined file
+cat logs/$1.csv | \
+    grep -v -i "^\"block" | \
+    sed 's/^/\"'$1'\",/' | \
+    tee -a combined/logs.csv
+
+cat logs/articulated/$1.csv | \
+    grep -v -i "^\"block" | \
+    sed 's/^/\"'$1'\",/' | \
+    tee -a combined/logs_articulated.csv
+
---
+++ gitcoin/scripts/combine_txs.1.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+# remove the header, add in a column for the address, append to combined file
+cat txs/$1.csv | \
+    grep -v -i "^\"block" | \
+    grep -v -i "^\"bn\",\"tx" | \
+    sed 's/^/\"'$1'\",/' | \
+    tee -a combined/txs.csv
