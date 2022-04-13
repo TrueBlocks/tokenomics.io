8a9fd1a446c463d068d2097cbab2967778306459
tjayrush <jrush@greathill.com>
2022-04-13 03:22:53 +0000

    Cleaning up

--- deploy_ui.sh
+++ deploy_ui.sh
@@ -6,7 +6,7 @@
 # build the site and deploy it to the website
 
 echo "Changing into the ui folder..."
-cd ui
+cd gitcoin/ui
 
 echo "Building..."
 yarn build
@@ -15,5 +15,18 @@ echo "Copying over the artifacts..."
 yes | cp -pr build/* /home/jrush/Websites/tokenomics.io/gitcoin/
 cd - 2>/dev/null
 yes | cp -pR charts/* /home/jrush/Websites/tokenomics.io/gitcoin/charts/
+
+echo "Done..."
+
+echo "Changing into the ui folder..."
+cd giveth/ui
+
+echo "Building..."
+yarn build
+
+echo "Copying over the artifacts..."
+yes | cp -pr build/* /home/jrush/Websites/tokenomics.io/giveth/
+cd - 2>/dev/null
+yes | cp -pR charts/* /home/jrush/Websites/tokenomics.io/giveth/charts/
 
 echo "Done..."
