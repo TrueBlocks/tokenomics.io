f4a4555f39b64a2530a5d0266b0a2523c041191b
Dawid Szlachta <git@makingprogress.dev>
2022-04-15 11:07:30 +0200

    Switched to TSV for addresses file

--- tools/cmd/combine.go
+++ tools/cmd/combine.go
@@ -5,10 +5,28 @@ Copyright © 2022 NAME HERE <EMAIL ADDRESS>
 package cmd
 
 import (
+	"errors"
 	"fmt"
+	"io"
+	"os"
+	"path"
 
+	tokenomics "github.com/TrueBlocks/tokenomics.io/pkg"
 	"github.com/spf13/cobra"
+	"github.com/spf13/pflag"
 )
+
+type FlagFileConfig struct {
+	InputDirectory string
+	OutputFile     string
+}
+
+var flagToFileConfig = map[string]FlagFileConfig{
+	"appearances": {
+		InputDirectory: "./apps", // TODO: change it to configurable source directory
+		OutputFile:     "./combined/apps.csv",
+	},
+}
 
 // combineCmd represents the combine command
 var combineCmd = &cobra.Command{
@@ -20,8 +38,66 @@ and usage of using your command. For example:
 Cobra is a CLI library for Go that empowers applications.
 This application is a tool to generate the needed files
 to quickly create a Cobra application.`,
-	Run: func(cmd *cobra.Command, args []string) {
+	RunE: func(cmd *cobra.Command, args []string) error {
 		fmt.Println("combine called")
+
+		addressesFilePath := args[0]
+
+		if cmd.Flags().NFlag() == 0 {
+			return errors.New("at least one flag required")
+		}
+
+		flagsSet := []string{}
+		cmd.Flags().Visit(func(f *pflag.Flag) {
+			flagsSet = append(flagsSet, f.Name)
+		})
+
+		reader, err := tokenomics.ReadGrants(addressesFilePath)
+		if err != nil {
+			return err
+		}
+
+		flagToOutputFiles := map[string]*os.File{}
+		for _, flagName := range flagsSet {
+			fileConfig, ok := flagToFileConfig[flagName]
+			if !ok {
+				return fmt.Errorf("missing file configuration for flag: %s", flagName)
+			}
+			output, err := os.OpenFile(fileConfig.OutputFile, os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0755)
+			if err != nil {
+				return err
+			}
+			flagToOutputFiles[flagName] = output
+			// runs at the end of the function
+			defer output.Close()
+		}
+
+		for {
+			grant, err := reader.Read()
+			if err == io.EOF {
+				break
+			}
+			if err != nil {
+				return err
+			}
+
+			for _, flagName := range flagsSet {
+				fileConfig := flagToFileConfig[flagName]
+
+				input, err := os.ReadFile(path.Join(fileConfig.InputDirectory, grant.Address) + ".csv")
+				if err != nil {
+					return err
+				}
+
+				output := flagToOutputFiles[flagName]
+				_, err = output.Write(input)
+				if err != nil {
+					return err
+				}
+			}
+		}
+
+		return nil
 	},
 }
 
@@ -37,4 +113,5 @@ func init() {
 	// Cobra supports local flags which will only run when this command
 	// is called directly, e.g.:
 	// combineCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+	combineCmd.Flags().BoolP("appearances", "a", false, "Output appearances file")
 }
---
+++ tools/pkg/addresses_file.go
@@ -0,0 +1,74 @@
+package tokenomics
+
+import (
+	"encoding/csv"
+	"fmt"
+	"io"
+	"os"
+
+	"github.com/TrueBlocks/tokenomics.io/pkg/types"
+	"github.com/TrueBlocks/trueblocks-core/src/apps/chifra/pkg/validate"
+)
+
+var requiredColumns = []string{
+	"address",
+	"name",
+}
+
+type GrantReader struct {
+	file      *os.File
+	header    map[string]int
+	csvReader csv.Reader
+}
+
+func (gr *GrantReader) Read() (types.Grant, error) {
+	record, err := gr.csvReader.Read()
+	if err == io.EOF {
+		gr.file.Close()
+	}
+	if err != nil {
+		return types.Grant{}, err
+	}
+	if !validate.IsValidAddress(record[0]) {
+		err = fmt.Errorf("not a valid address: %s", record[0])
+		return types.Grant{}, err
+	}
+
+	return types.Grant{
+		Address: record[gr.header["address"]],
+	}, nil
+}
+
+func ReadGrants(path string) (grantReader GrantReader, err error) {
+	file, err := os.Open(path)
+	if err != nil {
+		return
+	}
+
+	reader := csv.NewReader(file)
+	reader.Comma = '\t'
+
+	// read header
+	headerRow, err := reader.Read()
+	if err != nil {
+		return
+	}
+	header := map[string]int{}
+	for index, columnName := range headerRow {
+		header[columnName] = index
+	}
+	// make sure the header is correct
+	for _, required := range requiredColumns {
+		_, ok := header[required]
+		if !ok {
+			err = fmt.Errorf(`required column "%s" missing in file %s`, required, path)
+			return
+		}
+	}
+
+	return GrantReader{
+		file:      file,
+		header:    header,
+		csvReader: *reader,
+	}, nil
+}
