2422e4df654a2123fd659cd5c321f6d3a3205169
Thomas Jay Rush <jrush@quickblocks.io>
2022-04-22 21:57:41 +0200

    Updating updating scripts

---
+++ gitcoin/combine_apps.1.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+# remove the header, add in a column for the address, append to combined file
+cat apps/$1.csv | \
+    grep -v -i "^\"block" | \
+    sed 's/^/\"'$1'\",/' | \
+    tee -a combined/apps.csv
---
+++ gitcoin/combine_update.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+# chifra when --timestamps
+./combine_apps.sh
+./combine_txs.sh
+./combine_logs.sh
+./combine_neighbors.sh
+./combine_statements.sh
+
+cd combined
+rm -f *.gz
+yes | gzip *.csv
+
