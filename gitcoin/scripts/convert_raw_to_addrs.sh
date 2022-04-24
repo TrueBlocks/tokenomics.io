a84bc477097e532f97b26206afd480d772b78bf9
tjayrush <jrush@greathill.com>
2022-04-24 01:10:27 +0000

    Updating a script

--- gitcoin/scripts/convert_raw_to_addrs.sh
+++ gitcoin/scripts/convert_raw_to_addrs.sh
@@ -2,7 +2,8 @@
 
 # ./scripts/download_raw.sh
 
-cat gitcoin/exports/mainnet/raw/*.json | \
+cat ./exports/mainnet/raw/*.json | \
     sed 's/|/ /g' | \
     jq '.[] | "\(.admin_address)|\(.id)|\(.title)|\(.slug)|\(.active)"' | \
-    sed 's/"//g' | tr '|' '\t'
+    sed 's/"//g' | tr '|' '\t' | \
+    sort --ignore-case
